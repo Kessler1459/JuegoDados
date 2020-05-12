8ca23d73102e91bb295c4ba7199828de9f8e0e14
Kessler1459 <kessler1459@gmail.com>
2020-05-12 16:14:51 -0300

    menu principal parte

--- src/gui/app.java
+++ src/gui/app.java
@@ -1,15 +1,24 @@
 package gui;
 
-import clases.Dado;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 
 public class app {
     private JPanel panel1;
-    private Dado dad;
+    private JButton BtnDiezMil;
+    private JButton BtnGenerala;
+    private JLabel LTitulo;
+    private JLabel LJugadores;
+    private JLabel LJugador1;
+    private JLabel LJugador2;
+    private JTextField TxtFJugador2;
+    private JTextField TxtFJugador1;
+    private JLabel LJugador4;
+    private JLabel LJugador3;
+    private JTextField TxtFJugador3;
+    private JTextField TxtFJugador4;
 
 
     public static void main(String[] args) {
