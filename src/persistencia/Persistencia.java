8b8dbe11a75411c25b81c1d03483bd7b298dbf38
CamilaLM <66446940+CamilaLM09@users.noreply.github.com>
2020-06-25 15:23:38 -0300

    Update Persistencia.java
    Actualizacion de metodos

--- src/persistencia/Persistencia.java
+++ src/persistencia/Persistencia.java
@@ -7,32 +7,35 @@ import org.json.*;
 import clases.Juego;
 
 public class Persistencia {
-
+	
 	//Lo hacemos poli
 	private static final String game = "Generala";
-
+	
 	 public static void guardarPartida(Juego j)
 	    {
 	        JSONArray partidas=levantarArchivo();          //lee el array ya guardado en archivo
-	        partidas.put(To.ToJSON(j));                             //le agrega la partida nueva
+	        partidas.put(To.ToJSON(j));                            //le agrega la partida nueva
 	        escribirArray(partidas);                        //vuelve a escribir todo
 	    }
-
+	 
 	    public static JSONArray levantarArchivo()
 	    {
-	        JSONArray array =null;
+	    	JSONArray array =null;
 	        try {
-	            DataInputStream data = new DataInputStream(new FileInputStream("archivo"+game+".dat"));
-	            try {
-					array = new JSONArray(data.readUTF());
+	            	
+	        		DataInputStream data = new DataInputStream(new FileInputStream("archivo"+game+".dat"));
+
+					try {
+						array = new JSONArray(data.readUTF());
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
 					data.close();
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
+
 	        }
 	        catch (FileNotFoundException e)
 	        {
-	            JSONArray nuevo=new JSONArray();                  //si el archivo no existe, se crea un array vacio, se lo escribe vacio, y se retorna
+	        	JSONArray nuevo=new JSONArray();                  //si el archivo no existe, se crea un array vacio, se lo escribe vacio, y se retorna
 	            escribirArray(nuevo);
 	            return nuevo;
 	        }
@@ -42,10 +45,11 @@ public class Persistencia {
 	        }
 	        return array;                                       //sino, devuelve el array leido del archivo
 	    }
-
-	    public static void escribirArray(JSONArray ar)
+	 
+	    public static void escribirArray(JSONArray ar)          
 	    {
 	        DataOutputStream data=null;
+	    	   
 	        try
 	        {
 	            data=new DataOutputStream(new FileOutputStream("archivoGenerala.dat"));
